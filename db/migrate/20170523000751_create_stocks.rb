143b9567ff91fda2c5ec59234290cac8440f0d01
sensei100 <jenanton01@gmail.com>
2017-05-22 20:09:36 -0400

    generate stock scaffold

---
+++ db/migrate/20170523000751_create_stocks.rb
@@ -0,0 +1,10 @@
+class CreateStocks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stocks do |t|
+      t.string :symbol
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
